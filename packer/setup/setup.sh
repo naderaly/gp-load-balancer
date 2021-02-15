1ee25623dc253f1d49b3dd2bb3d17429ebda3e4e
Nader Ali <58347752+naderaly@users.noreply.github.com>
2021-02-15 13:59:39 +0200

    Update setup.sh

--- packer/setup/setup.sh
+++ packer/setup/setup.sh
@@ -37,6 +37,3 @@ stats auth username:password
 EOF
 sudo systemctl restart haproxy.service
 mv /tmp/setup/haproxy-update ~
-# This is a placeholder script, you can move your setup script here to install some custom deployment on the VM
-# The parent directory of this script will be transferred with its content to the VM under /tmp/setup path
-# (i.e: useful for copying configs, scripts, systemd units, etc..)  
